e817d4386b23a15845b7f77a4a8b295a4a3d36e7
shane o'moore <shaneomoore@shanes-iMac.lan>
2018-11-02 16:53:50 +0000

    Add Routing, Checkout & ContactData Pages

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,35 +4,34 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_,i)=>{
-                return <BurgerIngredient key={igKey+ i} type={igKey} />;
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
         })
-        .reduce((arr,el) => {
+        .reduce((arr, el) => {
             return arr.concat(el);
-        },[]);
+        }, []);
 
-    if(transformedIngredients.length === 0){
+    if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please start adding ingredients</p>;
     }
 
     /** Re-written my way :) */
 
     const myIngredients = [];
-    Object.keys(props.ingredients).forEach((v,i)=>{
+    Object.keys(props.ingredients).forEach((v, i) => {
         let j = 0;
-        while(j < props.ingredients[v]){
-            myIngredients.push(<BurgerIngredient key={v+ j} type={v} />);
+        while (j < props.ingredients[v]) {
+            myIngredients.push(<BurgerIngredient key={v + j} type={v} />);
             j++;
         }
     });
 
     /** My awesome code is above here */
 
-    return(
+    return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
             {transformedIngredients}
