004d2a756d90a5e4fe88722dd92ec56f91e97f41
shane o'moore <shaneomoore@shanes-iMac.lan>
2018-12-13 19:40:40 +0000

    Setup & Install Redux Packages

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions';
+
+const initState = {
+    ingredients: null,
+    totalPrice: 4,
+};
+
+const reducer = (state = initState, action) => {
+    return state;
+}
+
+export default reducer;
